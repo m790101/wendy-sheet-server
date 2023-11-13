f485a960aa919048fe3dbfe7d15f58f5b28cca17
鄭皓文 HaoWen Cheng <haowen.cheng@tpisoftware.com>
2023-11-13 16:42:01 +0800

    for netlify

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,20 +1,22 @@
 const express = require("express");
+const router = express.Router();
+import serverless from "serverless-http";
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.json())
 const mongoose = require('mongoose');
-const Item = require('./model/item')
+const Item = require('../../model/item')
 require('dotenv').config();
 
 // body header設置
 const {
     genResponse
-  } = require('./utils/responseHelper')
+  } = require('../../utils/responseHelper')
   const {
     successCode,
     getMessageByCode,
-  } = require('./config/index')
+  } = require('../../config/index')
 
 
 //mongodb 連線
@@ -37,12 +39,12 @@ const allowCrossDomain = (req, res, next) => {
 app.use(allowCrossDomain)
 
 
-app.get("/", async(req, res) => {
+router.get("/", async(req, res) => {
       res.send('yoooo')
 
 })
 
-app.get("/allItem", async(req, res) => {
+router.get("/allItem", async(req, res) => {
 
   let data = await Item.find()
   const resJsonData = genResponse(successCode, getMessageByCode(successCode), data)
@@ -50,7 +52,7 @@ app.get("/allItem", async(req, res) => {
 })
 
 
-app.post('/update', async (req, res) => { 
+router.post('/update', async (req, res) => { 
   const id = req.body.data._id
   const number = req.body.data.in_stock
   console.log('id',id)
@@ -69,7 +71,7 @@ app.post('/update', async (req, res) => {
 })
 
 
-app.post('/delete', async (req, res) => { 
+router.post('/delete', async (req, res) => { 
   const id = req.body.id
   console.log('body',req.body)
     try { 
@@ -85,7 +87,7 @@ app.post('/delete', async (req, res) => {
 })
 
 
-app.post('/add', async (req, res) => { 
+router .post('/add', async (req, res) => { 
   const data = req.body.data
   console.log('data',data)
     try { 
@@ -101,7 +103,9 @@ app.post('/add', async (req, res) => {
     }
 })
 
+app.use("/.netlify/functions/api", router);
+// app.listen(1337, (req, res) => console.log("app is running on http://localhost:1337/"));
+// db.on('error', console.error.bind(console, 'MongoDB connection error:')) // 連線失敗
+// db.once('open', (db) => console.log('Connected to MongoDB')); // 連線成功
 
-app.listen(1337, (req, res) => console.log("app is running on http://localhost:1337/"));
-db.on('error', console.error.bind(console, 'MongoDB connection error:')) // 連線失敗
-db.once('open', (db) => console.log('Connected to MongoDB')); // 連線成功
+module.exports.handler = serverless(app);
